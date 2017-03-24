d633c5eaa6143fb4b7afec6d61075627528252f5
Plínio Naves <pliniopjn@hotmail.com>
2017-03-24 11:44:43 -0300

    update to Angular 4

--- systemjs.config.js
+++ systemjs.config.js
@@ -21,7 +21,6 @@
       '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
       '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
       '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
-      '@angular/upgrade': 'npm:@angular/upgrade/bundles/upgrade.umd.js',
       // other libraries
       'rxjs':                      'npm:rxjs',
       'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js'
